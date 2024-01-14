0a169f761b22a9bd5573ce4286f64ad6cdc003c1
Justin Jemison <jjemi8884@gmail.com>
2024-01-13 19:59:13 -0800

    added the project folder and interface

---
+++ scr/Sort/SortInterface.java
@@ -0,0 +1,10 @@
+package Sort;
+
+public interface SortInterface {
+
+    /**
+     * The main call to sort the given array of integers
+     * @param array of integers to be sorted.
+     */
+    void sort(int[] array);
+}
