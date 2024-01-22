36f3ad6a614028f75c8ea6ef6980994c3fca57f8
Justin Jemison <jjemi8884@gmail.com>
2024-01-21 21:27:52 -0800

    good start to Quicksort, just more work

---
+++ scr/Sort/QuickSortMo3.java
@@ -0,0 +1,174 @@
+package Sort;
+
+import java.time.chrono.MinguoDate;
+
+public class QuickSortMo3 implements SortInterface{
+
+    /**
+     * Used for setting the size for when the insertion sort will take over from
+     * the quick sort. The lowest it can go is four, were it is not recommended to go above 10.
+     */
+    private static final int MAX_SIZE = 4;
+
+    /**
+     * The constructor will automatically take in the array and sort the array in the correct
+     * ascending order.
+     * @param array of integers in any pattern.
+     */
+    public QuickSortMo3(int[] array){
+        this.sort( array);
+    }//end QuickSortMo3
+
+    /**
+     * Helper method for the recursive sort call
+     * @param array of integers to be sorted.
+     */
+    public void sort(int [] array){
+        this.sort(0, array.length-1, array);
+    }
+
+    /**
+     * The main call to sort the given array of integers
+     * it first sees if the array is smaller than 4 then, if it
+     * is then the pivot method is not used and an insertion sort is used
+     * instead. The size of the array is dictated by the global variable MAX_SIZE.
+     * This is something to play around with and maybe can incorporate it into the constructor.
+     * BigO(n log n)
+     * @param array of integers to be sorted.
+     */
+    private void sort(int first, int last, int[] array) {
+
+        if((last - first) + 1 <= MAX_SIZE) {
+            insertionSort(first, last, array);
+        }else{
+            int pivotIndex = pivotSort(first, last, array);
+            sort(first, pivotIndex - 1, array);
+            sort(pivotIndex + 1, last, array);
+        }//end if
+    }//end sort
+
+    /**
+     * This method will take a pivot and sort all items less than the pivot to the left.
+     * All items that are larger than the pivot to the right and reinsert the pivot
+     * in its correct spot in the array by swapping with the next large number.
+     * @param firstIndex cannot equal zero and less than last index
+     * @param lastIndex less than array.lenght - 1
+     * @param array the pointer to the array that we are sorting
+     * @return the index to the pivot that we insert back in the array at the correct spot.
+     */
+    private int pivotSort(int firstIndex, int lastIndex, int [] array){
+        int pivotIndex = lastIndex/2;
+        mOThree(firstIndex,pivotIndex, lastIndex, array);
+
+        //putting the pivot in the second to last entry!
+        swap(pivotIndex, lastIndex -1, array);
+        //assume last index is already in the larger group due to Mo3
+        int fromLeftIndex  = firstIndex + 1; // start at +1 cause we know the pivot is larger form Mo3
+        int fromRightIndex = lastIndex - 2;
+        pivotIndex = lastIndex - 1;
+        int pivotValue = array[lastIndex - 1];
+        boolean done = false;
+        printArray(array);
+        while(!done) {
+            //move the fromleftIndex right until find a value higher than pivot
+            //should find one since it is
+            while (pivotValue > array[fromLeftIndex]) {
+                fromLeftIndex++;
+            }//end while
+
+            while (array[fromRightIndex] > pivotValue) {
+                fromRightIndex--;
+            }//end while
+
+            if (fromLeftIndex < fromRightIndex) {
+                swap(fromLeftIndex, fromRightIndex, array);
+                fromLeftIndex++;
+                fromRightIndex--;
+            } else {
+                done = true;
+            }//end if
+        }//end while
+        printArray(array);
+
+        swap(pivotIndex, fromLeftIndex, array);
+        pivotIndex = fromLeftIndex;
+        return pivotIndex;
+    }//end pivot sort
+
+    public static void printArray(int [] array){
+        for (int i = 0; i < array.length; i++){
+            System.out.print(array[i] + ", ");
+        }
+        System.out.println();
+    }
+
+    /**
+     * Method to inset in order because it is not wise to do the quicksort with less than four elements.
+     * @param first index of an array
+     * @param last index of the array
+     * @param array to be sorted.
+     */
+    private void insertionSort(int first, int last, int array []){
+       for(int unsorted = first + 1; unsorted <= last; unsorted++){
+           int nextNumberToInsert = array[unsorted];
+           insertInOrder(nextNumberToInsert,first, unsorted - 1, array);
+       }//for loop
+    }//end insertionSort
+
+    /**
+     * private method to be used for insertion sort when the array partition is less than 4
+     * @param firstIndex index in the array
+     * @param lastIndex index in the array
+     * @param array the array pointer for reference
+     */
+    private void insertInOrder(int value, int firstIndex, int lastIndex, int [] array){
+        //take the last index and make room to insert
+
+        int currentIndex = lastIndex;
+
+        while(currentIndex >= firstIndex && value < array[currentIndex]){
+            array[currentIndex + 1] = array[currentIndex];
+            currentIndex--;
+        }
+        array[currentIndex + 1] = value;
+
+    }
+
+    /**
+     * This method will choose the first, middle and last index of an array
+     * identify which of the three numbers are the median, then shift that pivot to the last index.
+     * all the while ordering the arrays in the correct order.
+     * Precondition - This assumes that there are at least three elements in the array.
+     * @param array of integers
+     */
+    private void mOThree(int first, int middle, int last, int [] array){
+
+        if(array[last] < array[first]) {
+            swap(last, first, array);
+        }
+        //known that first is smaller or last and first are the same
+        if(array[middle] < array[first]){
+            swap(middle, first, array);
+        }
+        //know that middle is smaller than first or they are the same
+        if(array[middle] > array[last]) {
+            swap(middle, last, array);
+        }
+        //know that middle array is smaller that last array
+
+
+    }//end mOThree
+
+    /**
+     * Method to easily swap elements in an array with BigO(1)
+     * @param first element to be swapped
+     * @param second element to be swapped
+     * @param array the array to swap the elements
+     */
+    private void swap(int first, int second, int [] array){
+        System.out.println("SWAP  " + array[first] + " and " + array[second] + "At indexs " + first + " frist " + second + " second");
+        int temp = array[second];
+        array[second] = array[first];
+        array[first] = temp;
+    }
+}//end class
--- scr/main.java
+++ scr/main.java
@@ -1,24 +1,41 @@
 import Sort.BucketSort;
+import Sort.QuickSortMo3;
 
 import java.util.Random;
 
 public class main {
     public static void main(String [] args){
-        int [] array = new int [20];
+        int [] array = new int [15];
         Random rand = new Random();
 
         for (int i = 0; i < array.length; i++){
-            int temp = rand.nextInt(40000);
+            int temp = rand.nextInt(100);
             array[i] = temp;
         }
-        for (int i = 0; i < array.length; i++){
-            System.out.print(array[i] + ", ");
-        }
-        System.out.println("");
-        BucketSort s = new BucketSort(array);
+        System.out.println("Original Array");
+        printArray(array);
 
+       // int [] bucketArray = array.clone();
+        int [] mO3Array = array.clone();
+        //System.out.println("");
+        //BucketSort s = new BucketSort(bucketArray);
+        //System.out.print("Bucket Sort Array\n");
+        //printArray(bucketArray);
+
+        QuickSortMo3 s1 = new QuickSortMo3(mO3Array);
+        System.out.println("QuickSortMo3 Array\n");
+        printArray(mO3Array);
+
+
+
+
+
+    }//end main
+
+    public static void printArray(int [] array){
         for (int i = 0; i < array.length; i++){
             System.out.print(array[i] + ", ");
         }
-    }//end main
+        System.out.println();
+    }
 }//end main class
