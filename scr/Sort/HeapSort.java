aa23ff7f286e3c16e4b7718181477e68df371985
Kette <Kettelcorn>
2024-01-23 14:22:16 -0800

    Finished logic for HeapSort, correctly sorts array

--- scr/Sort/HeapSort.java
+++ scr/Sort/HeapSort.java
@@ -8,29 +8,44 @@ public class HeapSort implements SortInterface{
 
     @Override
     public void sort(int[] array) {
+        buildMaxHeap(array);
+        int size = array.length;
 
+        for (int i = array.length - 1; i > 0; i--) {
+            int temp = array[i];
+            array[i] = array[0];
+            array[0] = temp;
+            size--;
+            heapify(array, size, 0);
+        }
     }
 
     public void buildMaxHeap(int[] array) {
-
+        for (int i = (array.length/2) - 1; i >= 0; i--) {
+            heapify(array, array.length, i);
+        }
     }
 
     public void heapify(int[] array, int size, int root) {
-        int leftChild = 2 * root + 1;
-        int rightChild = 2 * root + 2;
-        int largerChild = -1;
-        //TODO: logic for seeing if the parent has two children or not
-        if (array[leftChild] >= array[rightChild]) {
-            largerChild = leftChild;
-        } else {
-            largerChild = rightChild;
-        }
+        if (2 * root + 1 <= size - 1) {
+            int leftChild = 2 * root + 1;
+            int rightChild = 2 * root + 2;
+            int largerChild = -1;
 
-        if (array[largerChild] > array[root]) {
-            int temp = array[root];
-            array[root] = array[largerChild];
-            array[largerChild] = temp;
-            heapify(array, size, largerChild);
+            if (2 * root + 2 > size - 1) {
+                largerChild = leftChild;
+            } else if (array[leftChild] >= array[rightChild]) {
+                largerChild = leftChild;
+            } else {
+                largerChild = rightChild;
+            }
+
+            if (array[largerChild] > array[root]) {
+                int temp = array[root];
+                array[root] = array[largerChild];
+                array[largerChild] = temp;
+                heapify(array, size, largerChild);
+            }
         }
     }
 }
