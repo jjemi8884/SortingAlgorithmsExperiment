488e34497cd1c4e5861efb4fcf7dc9327ce19017
mx-cifu <cifuentesmax1@gmail.com>
2024-01-22 16:25:53 -0800

    Added needed JavaDoc

--- scr/Sort/InsertionSort.java
+++ scr/Sort/InsertionSort.java
@@ -2,6 +2,10 @@ package Sort;
 
 public class InsertionSort implements SortInterface {
 
+    /**
+     * Uses the passed in array to call a sort on the integers
+     * @param array an array of integers
+     */
     public InsertionSort(int[] array) {
         this.sort(array);
     }
@@ -14,17 +18,18 @@ public class InsertionSort implements SortInterface {
      */
     @Override
     public void sort(int[] array) {
+        // iteratively goes through array, sorting elements one by one
         for (int idx = 1; idx < array.length; idx++) {
             int currentVal = array[idx];
             int precedingIdx = idx - 1;
-
+            // moves current element to correct sorted place, going backwards until correct place
             while (precedingIdx >= 0 && array[precedingIdx] > currentVal) {
                 array[precedingIdx + 1] = array[precedingIdx];
                 precedingIdx--;
-            }
+            } // end while loop
             array[precedingIdx + 1] = currentVal;
-        }
+        } // end for loop
 
 
-    }
-}
+    }  // end sort method
+} // end class
--- scr/Sort/MergeSort.java
+++ scr/Sort/MergeSort.java
@@ -4,6 +4,10 @@ import java.util.Arrays;
 
 public class MergeSort implements SortInterface {
 
+    /**
+     * Uses the passed in array and calls a sort on it
+     * @param arrayOfInts an array of integers
+     */
     public MergeSort(int[] arrayOfInts) {
         this.sort(arrayOfInts);
     }
@@ -16,44 +20,57 @@ public class MergeSort implements SortInterface {
      */
     @Override
     public void sort(int[] array) {
+        // base case
         if (array.length >= 2) {
 
             int mid = array.length / 2;
             int[] leftHalf = Arrays.copyOfRange(array, 0, mid);
             int[] rightHalf = Arrays.copyOfRange(array, mid , array.length);
+            // recursively calls a new "half split" onto both halves
             sort(leftHalf);
             sort(rightHalf);
-
+            // takes those new "halves" and merges them, while sorting
             mergeArrays(leftHalf, rightHalf, array);
 
         }
     }
 
-    private void mergeArrays(int[] leftSide, int[] rightSide, int[] temp) {
+    /**
+     * A recursive helper method, used for merging split arrays and sorting them
+     * @param leftSide left half of a split
+     * @param rightSide right half of a split
+     * @param array reference to the "original" array that was split
+     */
+    private void mergeArrays(int[] leftSide, int[] rightSide, int[] array) {
             int leftIdx = 0;
             int rightIdx = 0;
 
+            // keep going while there are still elements in both that can be compared
             while (leftIdx < leftSide.length && rightIdx < rightSide.length) {
+                //if the element in the left-side array is less than what's in the right, set the next slot to left
+                // side's current value
                 if (leftSide[leftIdx] <= rightSide[rightIdx]) {
-                    temp[leftIdx + rightIdx] = leftSide[leftIdx];
+                    array[leftIdx + rightIdx] = leftSide[leftIdx];
                     leftIdx++;
+                    // right is smaller so do the opposite
                 } else {
-                    temp[leftIdx + rightIdx] = rightSide[rightIdx];
+                    array[leftIdx + rightIdx] = rightSide[rightIdx];
                     rightIdx++;
-                }
-            }
+                } // end else
+            } // end while
 
+            // add remaining elements of leftSide to the array
             while (leftIdx < leftSide.length) {
-                temp[leftIdx + rightIdx] = leftSide[leftIdx];
+                array[leftIdx + rightIdx] = leftSide[leftIdx];
                 leftIdx++;
             }
-
+            // add remaining elements of rightSide to the array
             while (rightIdx < rightSide.length) {
-                temp[leftIdx + rightIdx] = rightSide[rightIdx];
+                array[leftIdx + rightIdx] = rightSide[rightIdx];
                 rightIdx++;
             }
 
-    }
+    } // end mergeArrays
 
 
 }
